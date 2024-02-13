57d0ba315a7085e032eea47f28e32b2b4d848f39
otto18 <otto@ceramic.global>
2024-02-13 14:59:41 -0500

    add puppy list render

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-
+import { useState } from 'react'
+import {puppyList} from './data.js'
 
 function App() {
+  const [puppies, setPuppies] = useState(puppyList);
+
+  console.log("puppyList: ", puppyList);
 
   return (
     <>
-      
+      { 
+      puppies.map((puppy) => {
+      return <p key={puppy.id}>{puppy.name}</p>})
+      }
     </>
   )
 }
